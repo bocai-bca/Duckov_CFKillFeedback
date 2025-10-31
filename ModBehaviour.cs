e924a5c31106baa3f6b4739601fe5d72236b8d0d
Bocai_BCA <105545253+bocai-bca@users.noreply.github.com>
2025-11-01 04:14:47 +0800

    修了图标在不同分辨率下会变大变小的问题，给配置文件添加了一些选项

--- ModBehaviour.cs
+++ ModBehaviour.cs
@@ -14,20 +14,15 @@ namespace CFKillFeedback
 	{
 		public static bool Loaded = false;
 		public static Dictionary<string, object> DefaultConfig = new Dictionary<string, object>();
-		public static float IconStayAlpha = 0.75f;
-		public static float IconDropTime = 0.1f;
-		public static float IconStayTime = 1.0f;
-		public static float IconFadeoutTime = 1.25f;
 		public static Vector3 IconSizeDrop = new Vector3(4.0f, 4.0f);
 		public static Vector3 IconSizeStay = new Vector3(2.0f, 2.0f);
-		public static Vector2 IconSizeBaseMulti = new Vector2(960.0f, 540.0f);
+		//public static Vector2 IconSizeBaseMulti = new Vector2(960.0f, 540.0f);
 		//public static Vector3 IconPosPrctDrop = new Vector3(0.0f, -1.0f); //基于屏幕中心的百分比，1.0为一个半径
 		//public static Vector3 IconPosPrctStay = new Vector3(0.0f, -0.8f); //基于屏幕中心的百分比，1.0为一个半径
 		public static Vector3 IconPosPrctDrop = new Vector3(0.5f, 0.1f); //基于屏幕尺寸，原点为左下角
 		public static Vector3 IconPosPrctStay = new Vector3(0.5f, 0.2f); //基于屏幕尺寸，原点为左下角
 		public static float IconShakeLength = 0.1f; //基于半个屏幕尺寸中长和高之间较小的值
 		public static float IconShakeModSecond = 0.05f;
-		public const float ComboSeconds = 8.0f;
 		public static ModBehaviour? Instance;
 		// 所有图标名称，这些名称将用于拼接成定位图片资源的路径(.png)
 		public static readonly string[] IconNames = new string[]
@@ -75,38 +70,53 @@ namespace CFKillFeedback
 		public static bool simple_sfx = false;
 		// 配置文件-禁用图标，为true时不显示图标
 		public static bool disable_icon = false;
-        // 上次击杀的引擎启动时间，获取方式是Time.time
-        public static float last_kill_time = 0.0f;
+		// 配置文件-图标尺寸乘数
+		public static float icon_size_multi = 1.0f;
+		// 配置文件-图标不透明度
+		public static float icon_alpha = 0.75f;
+		// 配置文件-连杀计时
+		public static float combo_seconds = 8.0f;
+		// 配置文件-图标掉入时间
+		public static float icon_drop_seconds = 0.1f;
+		// 配置文件-图标停留时间
+		public static float icon_stay_seconds = 1.0f;
+		// 配置文件-图标淡出时间
+		public static float icon_fadeout_seconds = 1.25f;
+		// 配置文件-是否连杀图标优先级高于爆头
+		public static bool is_dont_use_headshot_icon_if_combo = false;
+		// 上次击杀的引擎启动时间，获取方式是Time.time
+		public static float last_kill_time = 0.0f;
 		// 连杀数
 		public static int combo_count = 0;
 		private void Update()
 		{
 			float delta = Time.time - last_kill_time;
-			Vector3 tr_pos = new Vector3();
 			if (ui_canvasgroup != null && ui_transform != null)
 			{
 				Vector2 screen_size = ui_transform.parent.position * 2.0f;
-				if (delta < IconDropTime)
+				if (delta < icon_drop_seconds)
 				{
-					float current_delta = Math.Clamp(delta / IconDropTime, 0.0f, 1.0f);
-					ui_canvasgroup.alpha = current_delta * IconStayAlpha;
-					ui_transform.localScale = Vector3.Lerp(IconSizeDrop, IconSizeStay, current_delta) * Math.Min(ui_transform.parent.position.x / IconSizeBaseMulti.x, ui_transform.parent.position.x / IconSizeBaseMulti.y);
+					float current_delta = Math.Clamp(delta / icon_drop_seconds, 0.0f, 1.0f);
+					ui_canvasgroup.alpha = current_delta * icon_alpha;
+					//ui_transform.localScale = Vector3.Lerp(IconSizeDrop, IconSizeStay, current_delta) * Math.Min(ui_transform.parent.position.x / IconSizeBaseMulti.x, ui_transform.parent.position.x / IconSizeBaseMulti.y) * icon_size_multi;
+					ui_transform.localScale = Vector3.Lerp(IconSizeDrop, IconSizeStay, current_delta) * icon_size_multi;
 					Vector3 pos = Vector3.Lerp(IconPosPrctDrop, IconPosPrctStay, current_delta);
-					tr_pos = new Vector3(pos.x * screen_size.x, pos.y * screen_size.y);
+					ui_transform.position = new Vector3(pos.x * screen_size.x, pos.y * screen_size.y);
 				}
-				else if (delta > IconDropTime + IconStayTime)
+				else if (delta > icon_drop_seconds + icon_stay_seconds)
 				{
-					ui_transform.localScale = IconSizeStay * Math.Min(ui_transform.parent.position.x / IconSizeBaseMulti.x, ui_transform.parent.position.x / IconSizeBaseMulti.y);
-					ui_canvasgroup.alpha = (1.0f - Math.Clamp((delta - IconDropTime - IconStayTime) / IconFadeoutTime, 0.0f, 1.0f)) * IconStayAlpha;
-                    tr_pos = new Vector3(IconPosPrctStay.x * screen_size.x, IconPosPrctStay.y * screen_size.y);
+					//ui_transform.localScale = IconSizeStay * Math.Min(ui_transform.parent.position.x / IconSizeBaseMulti.x, ui_transform.parent.position.x / IconSizeBaseMulti.y) * icon_size_multi;
+					ui_transform.localScale = IconSizeStay * icon_size_multi;
+					ui_canvasgroup.alpha = (1.0f - Math.Clamp((delta - icon_drop_seconds - icon_stay_seconds) / icon_fadeout_seconds, 0.0f, 1.0f)) * icon_alpha;
+					ui_transform.position = new Vector3(IconPosPrctStay.x * screen_size.x, IconPosPrctStay.y * screen_size.y);
 				}
 				else
 				{
-					ui_transform.localScale = IconSizeStay * Math.Min(ui_transform.parent.position.x / IconSizeBaseMulti.x, ui_transform.parent.position.x / IconSizeBaseMulti.y);
-					ui_canvasgroup.alpha = IconStayAlpha;
-					tr_pos = new Vector3(IconPosPrctStay.x * screen_size.x, IconPosPrctStay.y * screen_size.y);
+					//ui_transform.localScale = IconSizeStay * Math.Min(ui_transform.parent.position.x / IconSizeBaseMulti.x, ui_transform.parent.position.x / IconSizeBaseMulti.y) * icon_size_multi;
+					ui_transform.localScale = IconSizeStay * icon_size_multi;
+					ui_canvasgroup.alpha = icon_alpha;
+					ui_transform.position = new Vector3(IconPosPrctStay.x * screen_size.x, IconPosPrctStay.y * screen_size.y);
 				}
-				ui_transform.position = tr_pos;
 				if (disable_icon)
 				{
 					ui_canvasgroup.alpha = 0.0f;
@@ -125,10 +135,8 @@ namespace CFKillFeedback
 			{
 				if (KillFeedbackAudios_FMOD.ContainsKey("death"))
 				{
-					ChannelGroup channel_group;
-					RuntimeManager.GetBus("bus:/Master/SFX").getChannelGroup(out channel_group);
-					Channel channel = new Channel();
-					RuntimeManager.CoreSystem.playSound(KillFeedbackAudios_FMOD["death"], channel_group, false, out channel);
+					RuntimeManager.GetBus("bus:/Master/SFX").getChannelGroup(out ChannelGroup channel_group);
+					RuntimeManager.CoreSystem.playSound(KillFeedbackAudios_FMOD["death"], channel_group, false, out _);
 				}
 				return;
 			}
@@ -217,13 +225,16 @@ namespace CFKillFeedback
 				icon = KillFeedbackIcons["kill6"];
 				audio = KillFeedbackAudios_FMOD["kill"];
 			}
-			if (headshot) //爆头
+			if (combo_count > 1 && !is_dont_use_headshot_icon_if_combo)
 			{
-				icon = KillFeedbackIcons["headshot"];
-			}
-			if (goldheadshot) //黄金爆头
-			{
-				icon = KillFeedbackIcons["headshot_gold"];
+				if (headshot) //爆头
+				{
+					icon = KillFeedbackIcons["headshot"];
+				}
+				if (goldheadshot) //黄金爆头
+				{
+					icon = KillFeedbackIcons["headshot_gold"];
+				}
 			}
 			if (melee) //近战
 			{
@@ -245,11 +256,9 @@ namespace CFKillFeedback
 				}
 			}
 			// 应用资源
-			ChannelGroup channel_group;
-			RuntimeManager.GetBus("bus:/Master/SFX").getChannelGroup(out channel_group);
-			Channel channel;
-			RuntimeManager.CoreSystem.playSound(audio, channel_group, false, out channel);
-            channel.setVolume(volume);
+			RuntimeManager.GetBus("bus:/Master/SFX").getChannelGroup(out ChannelGroup channel_group);
+			RuntimeManager.CoreSystem.playSound(audio, channel_group, false, out Channel channel);
+			channel.setVolume(volume);
             if (ui_image != null && icon != null)
 			{
 				ui_image.sprite = Sprite.Create(icon, new Rect(0.0f, 0.0f, 512.0f, 512.0f), new Vector2(256.0f, 256.0f));
@@ -258,7 +267,7 @@ namespace CFKillFeedback
 		public static void UpdateCombo()
 		{
 			float time = Time.time;
-			if (time - last_kill_time > (float)ComboSeconds || time - last_kill_time <= 0.0f)
+			if (time - last_kill_time > (float)combo_seconds || time - last_kill_time <= 0.0f)
 			{
 				// 重置连杀数为第一杀
 				combo_count = 1;
@@ -275,7 +284,14 @@ namespace CFKillFeedback
 			DefaultConfig.TryAdd("volume", 1.0f);
 			DefaultConfig.TryAdd("simple_sfx", false);
 			DefaultConfig.TryAdd("disable_icon", false);
-            Instance = this;
+			DefaultConfig.TryAdd("icon_size_multi", 1.0f);
+			DefaultConfig.TryAdd("icon_alpha", 0.75f);
+			DefaultConfig.TryAdd("combo_seconds", 8.0f);
+			DefaultConfig.TryAdd("icon_drop_seconds", 0.1f);
+			DefaultConfig.TryAdd("icon_stay_seconds", 1.0f);
+			DefaultConfig.TryAdd("icon_fadeout_seconds", 1.25f);
+			DefaultConfig.TryAdd("is_dont_use_headshot_icon_if_combo", false);
+			Instance = this;
 			if (Loaded)
 			{
 				return;
@@ -319,7 +335,42 @@ namespace CFKillFeedback
                             disable_icon = (bool)property.Value;
                             continue;
                         }
-                    }
+						if (property.Name == "icon_size_multi" && property.Value.Type == JTokenType.Float)
+						{
+							icon_size_multi = (float)property.Value;
+							continue;
+						}
+						if (property.Name == "icon_alpha" && property.Value.Type == JTokenType.Float)
+						{
+							icon_alpha = (float)property.Value;
+							continue;
+						}
+						if (property.Name == "combo_seconds" && property.Value.Type == JTokenType.Float)
+						{
+							combo_seconds = (float)property.Value;
+							continue;
+						}
+						if (property.Name == "icon_drop_seconds" && property.Value.Type == JTokenType.Float)
+						{
+							icon_drop_seconds = (float)property.Value;
+							continue;
+						}
+						if (property.Name == "icon_stay_seconds" && property.Value.Type == JTokenType.Float)
+						{
+							icon_stay_seconds = (float)property.Value;
+							continue;
+						}
+						if (property.Name == "icon_fadeout_seconds" && property.Value.Type == JTokenType.Float)
+						{
+							icon_fadeout_seconds = (float)property.Value;
+							continue;
+						}
+						if (property.Name == "is_dont_use_headshot_icon_if_combo" && property.Value.Type == JTokenType.Boolean)
+						{
+							is_dont_use_headshot_icon_if_combo = (bool)property.Value;
+							continue;
+						}
+					}
                 }
 				else
 				{
@@ -347,7 +398,7 @@ namespace CFKillFeedback
 		{
 			UnityEngine.Debug.Log("CFKillFeedback: 开始加载资源/Starting loading resources");
 			bool success = true;
-			string absolute_path = Utils.GetDllDirectory();
+			string absolute_path = Path.Combine(Utils.GetDllDirectory(), "CustomPacks", "cf");
 			UnityEngine.Debug.Log("CFKillFeedback: Absolute path = " + absolute_path);
 			UnityEngine.Debug.Log("CFKillFeedback: 正在遍历图标名称列表/Foreaching IconNames list");
 			foreach (string icon_name in IconNames)
@@ -385,8 +436,7 @@ namespace CFKillFeedback
 				}
 				//UnityEngine.Debug.Log("CFKillFeedback: 将异步读取音频 = " + this_path);
 				//StartCoroutine(GetAudioClip("file:///" + this_path.Replace(" ", "%20"), audio_name));
-				Sound sound;
-				RESULT fmod_create_result = RuntimeManager.CoreSystem.createSound(this_path, MODE.LOOP_OFF, out sound);
+				RESULT fmod_create_result = RuntimeManager.CoreSystem.createSound(this_path, MODE.LOOP_OFF, out Sound sound);
 				if (fmod_create_result == RESULT.OK)
 				{
 					KillFeedbackAudios_FMOD.TryAdd(audio_name, sound);
